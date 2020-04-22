ea5cdccaec80223b8cc765d09417ec544a1f3d56
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2020-04-22 19:50:26 +0200

    Añadiendo los test de interfaz de usuario y los cambios realizados en el pom

---
+++ src/test/java/org/springframework/samples/petclinic/ui/HotelPositiveUITest.java
@@ -0,0 +1,107 @@
+
+package org.springframework.samples.petclinic.ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HotelPositiveUITest {
+
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/carlosjesusgarciaborrego/Downloads/Drivers/chromedriver");
+		this.driver = new ChromeDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testHotelPositiveUI() throws Exception {
+		this.driver.get("http://localhost:8080/petclinic/");
+		this.driver.findElement(By.xpath("//ul[2]/li/a")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("admin1");
+		this.driver.findElement(By.id("password")).click();
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("4dm1n");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.get("http://localhost:8080/petclinic/");
+		this.driver.findElement(By.xpath("//ul[1]/li[6]/a")).click();
+		this.driver.findElement(By.linkText("New Hotel")).click();
+		this.driver.findElement(By.id("name")).click();
+		this.driver.findElement(By.id("name")).clear();
+		this.driver.findElement(By.id("name")).sendKeys("Calle Betis");
+		this.driver.findElement(By.id("location")).clear();
+		this.driver.findElement(By.id("location")).sendKeys("Sevilla");
+		this.driver.findElement(By.id("capacity")).click();
+		this.driver.findElement(By.id("capacity")).clear();
+		this.driver.findElement(By.id("capacity")).sendKeys("10");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("(//a[contains(text(),'Show')])[2]")).click();
+		this.driver.findElement(By.id("location")).click();
+		this.driver.findElement(By.id("location")).clear();
+		this.driver.findElement(By.id("location")).sendKeys("Huelva");
+		this.driver.findElement(By.id("capacity")).click();
+		this.driver.findElement(By.id("capacity")).clear();
+		this.driver.findElement(By.id("capacity")).sendKeys("13");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("Delete")).click();
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
---
+++ src/test/java/org/springframework/samples/petclinic/ui/ReservationPositiveUITest.java
@@ -0,0 +1,99 @@
+
+package org.springframework.samples.petclinic.ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ReservationPositiveUITest {
+
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/carlosjesusgarciaborrego/Downloads/Drivers/chromedriver");
+		this.driver = new ChromeDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testRegistrationPositiveUI() throws Exception {
+		this.driver.get("http://localhost:8080/petclinic/");
+		this.driver.findElement(By.xpath("//ul[2]/li/a")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("owner1");
+		this.driver.findElement(By.id("password")).click();
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("0wn3r");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[4]/a/span[2]")).click();
+		this.driver.findElement(By.linkText("New Reservation")).click();
+		this.driver.findElement(By.id("telephone")).click();
+		this.driver.findElement(By.id("telephone")).clear();
+		this.driver.findElement(By.id("telephone")).sendKeys("666777888");
+		this.driver.findElement(By.id("reservationDate")).click();
+		this.driver.findElement(By.id("reservationDate")).clear();
+		this.driver.findElement(By.id("reservationDate")).sendKeys("2020/10/25");
+		this.driver.findElement(By.xpath("//body/div/div")).click();
+		new Select(this.driver.findElement(By.id("clinic"))).selectByVisibleText("clinica1");
+		this.driver.findElement(By.xpath("//option[@value='clinica1']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
