bb390e97a6739689b09880e508ac90834f093f63
Augusto Rivero <traco44@hotmail.com>
2020-05-30 22:31:48 +0200

    TestPerformance and TestUI modified
    Correcciones en test UI y creación de TestPerformance

--- src/test/java/org/springframework/samples/petclinic/ui/CauseNegativeUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/CauseNegativeUITest.java
@@ -1,23 +1,41 @@
 package org.springframework.samples.petclinic.ui;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CauseNegativeUITest {
 	
+	@LocalServerPort
+	private int				port;
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
+	  
+	  String pathToGeckoDriver = "C:\\Ingenieria de Software 19-20\\DP2\\";
+		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
+
+		
     driver = new FirefoxDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -25,7 +43,7 @@ public class CauseNegativeUITest {
 
   @Test
   public void CauseNegativeUI() throws Exception {
-    driver.get("http://localhost:8080/petclinic/");
+	  driver.get("http://localhost:" + this.port + "/petclinic/");
     driver.findElement(By.linkText("Login")).click();
     driver.findElement(By.xpath("//form[@action='/petclinic/login']")).click();
     driver.findElement(By.xpath("//div")).click();
@@ -71,12 +89,12 @@ public class CauseNegativeUITest {
     driver.findElement(By.xpath("//button[@type='submit']")).click();
   }
 
-  @After
+  @AfterEach
   public void tearDown() throws Exception {
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+      Assert.fail(verificationErrorString);
     }
   }
 
